ba5c87a1a5b5bd2514cf01515d753f16fde9497d
Nikhilps0 <91004869+Nikhilps0@users.noreply.github.com>
2022-06-19 21:00:37 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,6 @@ echo "LANG=en_US.utf-8" >> /etc/environment
 echo "LC_ALL=en_US.utf-8" >> /etc/environment
 service sshd restart
 
-echo "password123" | passwd root --stdin
 sed  -i 's/#PermitRootLogin yes/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 service sshd restart
